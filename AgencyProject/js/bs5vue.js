698772508a392b14f7d8cf2beb7814332c52141c
fuckingJohnso <sam920523sam@gmail.com>
2025-01-10 11:35:53 +0800

    1

--- AgencyProject/js/bs5vue.js
+++ AgencyProject/js/bs5vue.js
@@ -1,31 +1,58 @@
-const {createApp,ref}  = Vue;
+const { createApp, ref } = Vue;
 
-var myService = createApp({
+// var myService = createApp({
+//     data() {
+//         return{
+//             Services:[
+//                 {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
+//                 {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
+//             ]
+//         }
+//     }
+// }).mount("#services");
+
+
+// var vueProfolio = createApp({
+//     data() {
+//         return {
+//             Portfolio: []
+//         }
+//     }
+// }).mount("#portfolio")
+
+// $.ajax({
+//     url: "/portfolio",
+//     method: "get",
+//     dataType: "json",
+//     success: results => {
+//         vueProfolio.Portfolio = results;
+//     }
+
+// })
+
+var VueGallery = createApp({
     data() {
-        return{
-            Services:[
-                {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
-                {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
-            ]
+        return {
+        galleryData: [], // 存放作品資料的陣列
         }
-    }
-}).mount("#services");
+    },
+    mounted() {
+        // 在實例被創建之後執行，通常用來發送獲取資料的請求
+        this.fetchgalleryData();
 
-
-var vueProfolio = createApp({
-    data() {
-        return{
-            Portfolio:[]
-        }
-    }
-}).mount("#portfolio")
-
-$.ajax({
-    url:"/profolio",
-    method: "get",
-    dataType: "json",
-    success: results=>{
-        vueProfolio.Portfolio = results;
-    }
-
-})
+    },
+    methods: {
+        fetchgalleryData() {
+            // 使用 Ajax 或其他方法向後端發送請求，並將資料設置到 works 中
+            // 這裡假設你的後端提供了一個 /api/works 的 API
+            fetch("/glry")
+                .then((response) => response.json())
+                .then((data) => {
+                    this.galleryData = data; // 將後端傳回的資料設置到 works 中
+                })
+                .catch((error) => {
+                    console.error("Error fetching galley data:", error);
+                });
+        },
+    },
+}).mount("#gallerysc");
